60abcec67cd166f3cae26b503d13adc1f33e72c5
O. Odler, 577 <oodler@cpan.org>
2024-09-06 02:44:04 +0000

    implemented 'random' - got 2 more to go

---
+++ lib/Acme/Free/Public/APIs.pm
@@ -0,0 +1,89 @@
+package Acme::Free::Public::APIs;
+
+use strict;
+use warnings;
+
+our $VERSION = '1.0.0';
+
+use HTTP::Tiny;
+use JSON            qw/decode_json/;
+use Util::H2O::More qw/baptise ddd HTTPTiny2h2o/;
+
+use constant {
+    BASEURL => "https://www.freepublicapis.com/api/",
+};
+
+sub new {
+    my $pkg  = shift;
+    my $self = baptise { ua => HTTP::Tiny->new }, $pkg;
+    return $self;
+}
+
+sub random {
+    my $self = shift;
+    my $URL  = sprintf "%s/random", BASEURL;
+    my $resp = HTTPTiny2h2o $self->ua->get($URL);
+    return $resp->content;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Acme::Free::Public::APIs - Perl API client for ...
+
+This module provides the client, "freeapis", that is available via C<PATH> after install.
+
+=head1 SYNOPSIS
+
+  #!/usr/bin/env perl
+    
+  use strict;
+  use warnings;
+  
+  use Acme::Free::Public::APIs qw//;
+
+  my $api     = Acme::Free::Public::APIs->new->random;
+  my $out     = <<EOAPI;
+  id:            %d
+  title:         %s%s
+  site URL:      %s
+  methods:       %s 
+  health:        %d 
+  documentation: %s
+  description:   %s
+  EOAPI
+  printf $out, $api->id, ($api->emoji) ? sprintf("(%s) ",$api->emoji) : "",, $api->title, $api->source, $api->methods, $api->health, $api->documentation, $api->source;
+
+=head2 C<freeapis> Commandline Client
+
+After installing this module, simply run the command C<freeapis> without any arguments, and it will print
+a random Kanfreeapis Rest quote to C<STDOUT>.
+
+  shell> freeapis
+  id:            156
+  title:         (🔑) KeyVal API
+  site URL:      https://freepublicapis.com/keyval-api
+  methods:       2 
+  health:        90 
+  documentation: https://keyval.org
+  description:   https://freepublicapis.com/keyval-api
+  shell>
+
+=head1 ENVIRONMENT
+
+Nothing special required.
+
+=head1 AUTHOR
+
+Brett Estrade L<< <oodler@cpan.org> >>
+
+=head1 BUGS
+
+Please report.
+
+=head1 LICENSE AND COPYRIGHT
+
+Same as Perl/perl.
